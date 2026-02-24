ab98807f1efd9ce06da2a705ed985c0ac9b3280e
Rajib Singh <connectedtorajib@gmail.com>
2026-02-24 12:35:41 +0000

    add the firebase cooection to connect this app with the project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-project-id.firebaseapp.com",
-  projectId: "placeholder-project-id",
-  storageBucket: "placeholder-project-id.appspot.com",
-  messagingSenderId: "placeholder-sender-id",
-  appId: "placeholder-app-id",
+  apiKey: "AIzaSyAF8KOh-oBre6zHSO6lu-A6AlvUe6kLF5k",
+  authDomain: "studio-4941900787-9d9c2.firebaseapp.com",
+  projectId: "studio-4941900787-9d9c2",
+  storageBucket: "studio-4941900787-9d9c2.firebasestorage.app",
+  messagingSenderId: "271962847668",
+  appId: "1:271962847668:web:840c4ee8b028831b305ae1"
 };
